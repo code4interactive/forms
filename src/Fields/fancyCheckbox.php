e3bcae656d207da4d4035cf5142cc3b4ebf57f88
Artur Bartczak <artur.bartczak@code4.pl>
2015-12-17 16:49:42 +0100

    Fancy checkbox

---
+++ src/Fields/fancyCheckbox.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Code4\Forms\Fields;
+
+use Code4\Forms\Traits\checkedTrait;
+use Code4\Forms\Traits\groupFieldTrait;
+
+class fancyCheckbox extends checkbox {
+    protected $_view = 'fancyCheckbox';
+}
---
+++ views/fancyCheckbox.blade.php
@@ -0,0 +1,5 @@
+<div class="checkbox checkbox-primary">
+    <input data-field-name="{{$el->getDotNotatedName()}}" type="{{$el->_type()}}" name="{{$el->name()}}" value="{{$el->value()}}" {!!$el->checked()!!} {!! $el->attributes() !!}>
+    <label>@if($el->fieldLabel()) {!! $el->fieldLabel() !!} @endif</label>
+</div>
+<div data-field-name="{{$el->getDotNotatedName()}}" data-name="{{$el->name()}}" class="error-container"></div>
